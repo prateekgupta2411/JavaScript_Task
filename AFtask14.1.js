9bd34d974656f44c15922dc139dc190d4da312e3
prateekgupta2411 <prateek.gupta_cs19@gla.ac.in>
2023-07-04 16:55:40 +0530

    AFtask14

---
+++ AFtask14.1.js
@@ -0,0 +1,11 @@
+const array = [
+    {"name":"Jack","maths":55,"english":60,"science":62},
+    {"name":"Anita","maths":62,"english":65,"science":56},
+    {"name":"Thomas","maths":68,"english":58,"science":54},
+    {"name":"Steve","maths":51,"english":56,"science":68},
+    {"name":"Julia","maths":47,"english":77,"science":72},
+    {"name":"Mary","maths":72,"english":55,"science":60}
+];
+array.sort((a, b) => a.name.localeCompare(b.name));
+
+console.log(array);
---
+++ AFtask14.2.js
@@ -0,0 +1,11 @@
+const data = [
+    {"name":"Jack","maths":55,"english":60,"science":62},
+    {"name":"Anita","maths":62,"english":65,"science":56},
+    {"name":"Thomas","maths":68,"english":58,"science":54},
+    {"name":"Steve","maths":51,"english":56,"science":68},
+    {"name":"Julia","maths":47,"english":77,"science":72},
+    {"name":"Mary","maths":72,"english":55,"science":60}
+];
+data.sort((a, b) => b.name.localeCompare(a.name));
+
+console.log(data);
